6f04907f624d67ec140ffd4c3e1cb13b321b9e97
Jan Mönnich <jan.moennich@gmx.de>
2015-08-05 14:14:35 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+
+var version = '1.0';
+var express = require('express');
+var requestify = require('requestify');
+var app = express();
+
+app.get('/', function (req, res) {
+
+  res.set('Content-Type', 'text/plain');
+
+  requestify.get('http://169.254.169.254/latest/meta-data/instance-id', {timeout: 2000}).then(
+    function (meta) {
+      res.send('this is version ' + version + ' on instance ' + meta.body + '\n').end();
+    },
+    function () {
+      res.send('this is version ' + version + '\n').end();
+    }
+  );
+
+});
+
+app.listen(80);
+
